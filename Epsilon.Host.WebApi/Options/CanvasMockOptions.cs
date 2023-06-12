c178e39ef66869f9519a7aa38f6f175caef0518c
Jelle Maas <typiqally@gmail.com>
2023-06-12 14:02:04 +0200

    Add initial Web API project to solution

---
+++ Epsilon.Host.WebApi/Options/CanvasMockOptions.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Epsilon.Host.WebApi.Options;
+
+/// <summary>
+/// A temporary mock class that is used to hold Canvas options,
+/// should be removed after proper LTI support is added into the system, after which this becomes obsolete 
+/// </summary>
+public class CanvasMockOptions
+{
+    [Required]
+    public Uri ApiUrl { get; set; } = null!;
+
+    [Required]
+    public int CourseId { get; set; }
+
+    [Required]
+    public int StudentId { get; set; }
+
+    [Required(AllowEmptyStrings = false)]
+    public string AccessToken { get; set; } = string.Empty;
+}
---
+++ Epsilon.Host.WebApi/Program.cs
@@ -0,0 +1,70 @@
+using System.Net.Http.Headers;
+using Epsilon.Canvas.Abstractions;
+using Epsilon.Canvas.Abstractions.GraphQl;
+using Epsilon.Canvas.Abstractions.Rest;
+using Epsilon.Canvas.GraphQl;
+using Epsilon.Canvas.Rest;
+using Epsilon.Host.WebApi.Options;
+using Microsoft.Extensions.Options;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Add CORS rules
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy => policy.WithOrigins(builder.Configuration["Lti:TargetUri"]).AllowCredentials()));
+
+// Add controllers
+builder.Services.AddControllers();
+
+// Add Canvas services
+const string canvasHttpClient = "CanvasHttpClient";
+var canvasConfiguration = builder.Configuration.GetSection("Canvas");
+
+builder.Services.Configure<CanvasMockOptions>(canvasConfiguration);
+builder.Services.AddHttpClient(
+    canvasHttpClient,
+    static (provider, client) =>
+    {
+        var settings = provider.GetRequiredService<IOptions<CanvasMockOptions>>().Value;
+
+        client.BaseAddress = settings.ApiUrl;
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", settings.AccessToken);
+    });
+
+builder.Services.AddScoped<ICanvasRestApi, CanvasRestApi>();
+builder.Services.AddHttpClient<ICanvasGraphQlApi, CanvasGraphQlApi>(canvasHttpClient);
+
+builder.Services.AddHttpClient<IPageEndpoint, PageEndpoint>(canvasHttpClient);
+builder.Services.AddHttpClient<IFileEndpoint, FileEndpoint>(canvasHttpClient);
+builder.Services.AddHttpClient<IAccountEndpoint, AccountEndpoint>(canvasHttpClient);
+
+builder.Services.AddScoped<CanvasUserSession>(static services =>
+{
+    var options = services.GetRequiredService<IOptions<CanvasMockOptions>>().Value;
+    return new CanvasUserSession(options.CourseId, options.StudentId, options.AccessToken);
+});
+
+// Add Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwaggerUI();
+}
+
+app.UseSwagger();
+
+app.UseHttpsRedirection();
+
+app.UseCors();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
