3c3117edf556f35b2c456b370a20fc72a5c7e62e
Tim <timbrouwers1@gmail.com>
2023-06-14 15:15:14 +0200

    Structure interface

--- Epsilon.Canvas.Abstractions/Rest/IPageEndpoint.cs
+++ Epsilon.Canvas.Abstractions/Rest/IPageEndpoint.cs
@@ -2,19 +2,19 @@ namespace Epsilon.Canvas.Abstractions.Rest;
 
 public interface IPageEndpoint
 {
+    Task<Page?> GetPage(int courseId, string id);
+    
+    Task<IEnumerable<Page>?> GetAll(int courseId, IEnumerable<string> include);
+
+    Task<Page?> CreatePage(int courseId, Page page);
+
+    Task<Page?> UpdatePage(int courseId, Page page);
+    
+    Task<Page?> UpdateOrCreatePage(int courseId, Page page);
+
     Task<IEnumerable<PageRevision>?> GetRevisions(int courseId, string pageId);
 
     Task<PageRevision?> GetRevision(int courseId, string pageId, int revisionId);
     
     Task<PageRevision?> GetClosestRevisionByDates(int courseId, string pageId, DateTime startDate, DateTime endDate);
-
-    Task<Page?> UpdateOrCreatePage(int courseId, string id, string html);
-
-    Task<Page?> CreatePage(int courseId, string id, string html);
-
-    Task<Page?> UpdatePage(int courseId, string id, string html);
-    
-    Task<Page?> GetPage(int courseId, string id);
-
-    Task<IEnumerable<Page>?> GetAll(int courseId, IEnumerable<string> include);
 }
