46f4baadbbaddea96a166b2ca4501ae3b3461e57
Jelle Maas <typiqally@gmail.com>
2023-06-15 13:09:12 +0200

    Make body required

--- Epsilon.Host.WebApi/Models/PageUpdateRequest.cs
+++ Epsilon.Host.WebApi/Models/PageUpdateRequest.cs
@@ -1,7 +1,8 @@
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace Epsilon.Host.WebApi.Models;
 
 public record PageUpdateRequest(
-    [property: JsonPropertyName("body")] string? Body
+    [property: JsonPropertyName("body"), Required,] string? Body
 );
