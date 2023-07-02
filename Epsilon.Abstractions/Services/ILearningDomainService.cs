c5fb4cc79bf8577919eb007cfd721168c915e93b
Jelle Maas <typiqally@gmail.com>
2023-07-02 14:21:20 +0200

    Add GetOutcomes method to learning domain service

--- Epsilon.Abstractions/Services/ILearningDomainService.cs
+++ Epsilon.Abstractions/Services/ILearningDomainService.cs
@@ -3,4 +3,6 @@ namespace Epsilon.Abstractions.Services;
 public interface ILearningDomainService
 {
     public Task<LearningDomain?> GetDomain(string name);
+
+    public Task<IEnumerable<LearningDomainOutcome?>> GetOutcomes();
 }
