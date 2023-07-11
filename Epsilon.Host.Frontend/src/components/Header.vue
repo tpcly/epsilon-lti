6954ada409019b7e736c1bd697ad77b3ed2420d6
Jelle Maas <typiqally@gmail.com>
2023-07-11 20:28:59 +0200

    Add student selection using accessible student filter

--- Epsilon.Host.Frontend/src/components/Header.vue
+++ Epsilon.Host.Frontend/src/components/Header.vue
@@ -4,8 +4,12 @@
         <Row>
             <Col :cols="9">
                 <div class="d-flex">
-                    <img :src="DefaultAvatar" class="avatar-image" alt="test" />
-                    <SearchBox v-model="undefined" :items="[]" :limit="5" />
+                    <img :src="avatarImage" class="avatar-image" alt="test" />
+                    <SearchBox
+                        v-model="selectedStudent"
+                        :items="students"
+                        placeholder="Student"
+                        :limit="5" />
                 </div>
             </Col>
             <Col :cols="3">
@@ -25,21 +29,48 @@ import SearchBox from "@/components/SearchBox.vue"
 import Row from "@/components/Row.vue"
 import Col from "@/components/Col.vue"
 
-import { inject, onMounted, Ref, ref } from "vue"
-import { Api, EnrollmentTerm, HttpResponse } from "@/api"
+import { computed, inject, onMounted, Ref, ref, watch } from "vue"
+import { Api, EnrollmentTerm, HttpResponse, User } from "@/api"
 
 const api = inject<Api<unknown>>("api")
+
+const students: Ref<User[]> = ref([])
+const selectedStudent: Ref<User | undefined> = ref(undefined)
 
 const terms: Ref<EnrollmentTerm[]> = ref([])
 const selectedTerm: Ref<EnrollmentTerm | undefined> = ref(undefined)
 
 onMounted(() => {
+    api?.filter.accessibleStudentsList().then((r: HttpResponse<User[]>) => {
+        students.value = r.data
+        selectedStudent.value = students.value[0]
+    })
+})
+
+watch(selectedStudent, () => {
+    if (!selectedStudent.value?._id) {
+        return
+    }
+
+    // Reset current term list
+    terms.value = []
+
     api?.filter
-        .participatedTermsList()
+        .participatedTermsList({
+            studentId: selectedStudent.value?._id,
+        })
         .then((r: HttpResponse<EnrollmentTerm[]>) => {
             terms.value = r.data
             selectedTerm.value = terms.value[0]
         })
+})
+
+const avatarImage = computed(() => {
+    if (!selectedStudent.value?.avatarUrl) {
+        return DefaultAvatar
+    }
+
+    return selectedStudent.value?.avatarUrl
 })
 </script>
 
