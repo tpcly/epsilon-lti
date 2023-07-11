0e02c4341c537344a4806562f0ba0e8cfe689ea3
Jelle Maas <typiqally@gmail.com>
2023-07-11 20:25:25 +0200

    Add enrollments and users to GraphQL models

--- Epsilon.Canvas.Abstractions/GraphQl/Course.cs
+++ Epsilon.Canvas.Abstractions/GraphQl/Course.cs
@@ -4,5 +4,6 @@ namespace Epsilon.Canvas.Abstractions.GraphQl;
 
 public record Course(
     [property: JsonPropertyName("name")] string? Name,
-    [property: JsonPropertyName("submissionsConnection")] GraphQlConnection<Submission>? Submissions
+    [property: JsonPropertyName("submissionsConnection")] GraphQlConnection<Submission>? Submissions,
+    [property: JsonPropertyName("enrollmentsConnection")] GraphQlConnection<Enrollment>? Enrollments
 );
