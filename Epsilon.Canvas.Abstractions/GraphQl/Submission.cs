506cb62da39acdb973da93a0882062a9746badfd
Jelle Maas <typiqally@gmail.com>
2023-07-22 14:18:36 +0200

    Add missing submission property (#10)

--- Epsilon.Canvas.Abstractions/GraphQl/Submission.cs
+++ Epsilon.Canvas.Abstractions/GraphQl/Submission.cs
@@ -4,6 +4,7 @@ namespace Epsilon.Canvas.Abstractions.GraphQl;
 
 public record Submission(
     [property: JsonPropertyName("postedAt")] DateTime? PostedAt,
+    [property: JsonPropertyName("submittedAt")] DateTime? SubmittedAt,
     [property: JsonPropertyName("assignment")] Assignment? Assignment,
     [property: JsonPropertyName("submissionHistoriesConnection")] GraphQlConnection<SubmissionHistory>? SubmissionHistories,
     [property: JsonPropertyName("rubricAssessmentsConnection")] GraphQlConnection<RubricAssessment>? RubricAssessments
