f0e45c184533913eae84a5cc8d2a3d4cb1c48dcd
Neal Geilen <info@nealgeilen.nl>
2023-07-22 15:48:23 +0200

    Changes endpoint

---
+++ Epsilon.Host.WebApi/Controllers/LearningController.cs
@@ -0,0 +1,40 @@
+using Epsilon.Abstractions;
+using Epsilon.Abstractions.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Epsilon.Host.WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class LearningController : ControllerBase
+{
+    private readonly ILearningDomainService _learningDomainService;
+    private readonly ILearningOutcomeCanvasResultService _learningOutcomeCanvasResultService;
+
+
+    public LearningController(ILearningDomainService learningDomainService, ILearningOutcomeCanvasResultService learningOutcomeCanvasResultService)
+    {
+        _learningDomainService = learningDomainService;
+        _learningOutcomeCanvasResultService = learningOutcomeCanvasResultService;
+    }
+
+    [HttpGet("outcomes")]
+    public async Task<ActionResult<LearningDomain>> GetResults(string studentId)
+    {
+        var outcomes = _learningOutcomeCanvasResultService.GetOutcomeResults(studentId);
+
+        return Ok(outcomes);
+    }
+
+    [HttpGet("domain/{name}")]
+    public async Task<ActionResult<LearningDomain>> GetName(string name)
+    {
+        var domain = await _learningDomainService.GetDomain(name);
+        if (domain == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(domain);
+    }
+}
--- Epsilon.Host.WebApi/Controllers/PageController.cs
+++ Epsilon.Host.WebApi/Controllers/PageController.cs
@@ -6,16 +6,16 @@ namespace Epsilon.Host.WebApi.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class ComponentController : ControllerBase
+public class PageController : ControllerBase
 {
     private readonly IPageComponentManager _pageComponentManager;
 
-    public ComponentController(IPageComponentManager pageComponentManager)
+    public PageController(IPageComponentManager pageComponentManager)
     {
         _pageComponentManager = pageComponentManager;
     }
 
-    [HttpGet("page/{pageName}")]
+    [HttpGet("{pageName}")]
     public async Task<ActionResult<PageComponent>> GetPage(int courseId, string pageName)
     {
         var pageComponent = await _pageComponentManager.Fetch(courseId, pageName);
@@ -23,7 +23,7 @@ public class ComponentController : ControllerBase
         return Ok(pageComponent);
     }
 
-    [HttpPost("page/{pageName}")]
+    [HttpPost("{pageName}")]
     public async Task<ActionResult<PageComponent>> UpdateOrCreatePage(int courseId, string pageName, [FromBody] PageUpdateRequest updateRequest)
     {
         var createdPageComponent = await _pageComponentManager.CreateOrUpdate(courseId, pageName, updateRequest.Body);
