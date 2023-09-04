f158fc42980e48b820563a9cc7adfe2a4852766f
Neal Geilen <info@nealgeilen.nl>
2023-09-04 14:55:02 +0200

    Added Competence profile

---
+++ Epsilon.Host.Frontend/src/components/Competence/CompetenceProfileCell.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { defineProps } from "vue"
+import { LearningDomainOutcome } from "@/api"
+
+defineProps<{
+    result: LearningDomainOutcome | null
+}>()
+</script>
+
+<template>
+    <td :style="{ background: '#' + result?.value?.hexColor }"></td>
+</template>
+
+<style scoped lang="scss">
+td {
+    text-align: center;
+}
+</style>
--- Epsilon.Host.Frontend/src/views/CompetenceDocument.vue
+++ Epsilon.Host.Frontend/src/views/CompetenceDocument.vue
@@ -1,4 +1,5 @@
 <template>
+    <CompetenceProfile></CompetenceProfile>
     <KpiTable></KpiTable>
     <KpiMatrix></KpiMatrix>
 </template>
@@ -6,4 +7,5 @@
 <script lang="ts" setup>
 import KpiMatrix from "@/components/Competence/KpiMatrix.vue"
 import KpiTable from "@/components/Competence/KpiTable.vue"
+import CompetenceProfile from "@/components/Competence/CompetenceProfile.vue"
 </script>
