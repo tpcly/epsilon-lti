341c5eb4e089cf1ad5870c2d340c317b0fea70c8
Neal Geilen <info@nealgeilen.nl>
2023-09-04 15:58:04 +0200

    Copied all needed code from older project

---
+++ Epsilon.Host.Frontend/src/components/CompetenceGraph.vue
@@ -0,0 +1,122 @@
+<template>
+    <ApexChart
+        :options="chartOptions"
+        :series="series"
+        height="300"
+        type="bar"
+        class="competence-graph"
+        width="490" />
+</template>
+
+<script lang="ts" setup>
+import ApexChart from "vue3-apexcharts"
+import { IHboIDomain, ProfessionalTaskResult } from "../logic/Api"
+import { onMounted, watchEffect } from "vue"
+import {
+    DecayingAverageLogic,
+    DecayingAveragePerLayer,
+} from "../logic/DecayingAverageLogic"
+
+let series: Array<{
+    name: string
+    color: string
+    data: Array<string | number> | undefined
+}> = []
+const chartOptions = {
+    annotations: {
+        yaxis: [
+            {
+                y: 3,
+                borderColor: "red",
+                strokeDashArray: 0,
+                label: {
+                    borderColor: "red",
+                    style: {
+                        color: "#fff",
+                        background: "red",
+                    },
+                    text: "Mastery",
+                },
+            },
+        ],
+    },
+    colors: [],
+    chart: {
+        type: "bar",
+        height: 300,
+        stacked: true,
+        toolbar: {
+            show: true,
+        },
+        zoom: {
+            enabled: false,
+        },
+    },
+    dataLabels: {
+        enabled: false,
+    },
+    plotOptions: {
+        bar: {
+            horizontal: false,
+            borderRadius: 4,
+            dataLabels: {},
+        },
+    },
+    xaxis: {
+        type: "string",
+        categories: [],
+    },
+    yaxis: {
+        show: false,
+    },
+    legend: {
+        show: false,
+    },
+    fill: {
+        opacity: 1,
+    },
+    tooltip: {
+        enabled: true,
+    },
+}
+
+const props = defineProps<{
+    domain: IHboIDomain
+    data: ProfessionalTaskResult[]
+}>()
+
+function loadChartData(): void {
+    series = []
+    chartOptions.xaxis.categories = []
+    if (props.domain.activities != null) {
+        props.domain.activities.forEach((s) => {
+            chartOptions.xaxis.categories.push(s.shortName as never)
+        })
+    }
+
+    DecayingAverageLogic.getAverageTaskOutcomeScores(
+        props.data,
+        props.domain
+    ).map((layer: DecayingAveragePerLayer) => {
+        const ar = props.domain.architectureLayers?.at(layer.architectureLayer)
+        series.push({
+            name: ar?.name,
+            color: ar?.color,
+            data: layer.layerActivities.map((ac) =>
+                ac.decayingAverage.toFixed(3)
+            ),
+        })
+    })
+}
+
+onMounted(() => {
+    loadChartData()
+})
+watchEffect(() => loadChartData())
+</script>
+
+<style scoped>
+.competence-graph {
+    margin-left: auto;
+}
+</style>
---
+++ Epsilon.Host.Frontend/src/components/PersonalDevelopmentGraph.vue
@@ -0,0 +1,122 @@
+<template>
+    <ApexChart
+        :options="chartOptions"
+        :series="series"
+        height="300"
+        type="bar"
+        width="200" />
+</template>
+
+<script lang="ts" setup>
+import ApexChart from "vue3-apexcharts"
+import {
+    IHboIDomain,
+    MasteryLevel,
+    ProfessionalSkillResult,
+} from "../logic/Api"
+import { onMounted, watchEffect } from "vue"
+import { DecayingAverageLogic } from "../logic/DecayingAverageLogic"
+
+const props = defineProps<{
+    domain: IHboIDomain
+    data: ProfessionalSkillResult[]
+}>()
+
+let series: Array<{ name: string; data: Array<number | string> }> = []
+const chartOptions = {
+    annotations: {
+        yaxis: [
+            {
+                y: 3,
+                borderColor: "red",
+                strokeDashArray: 0,
+                label: {
+                    borderColor: "red",
+                    style: {
+                        color: "#fff",
+                        background: "red",
+                    },
+                    text: "Mastery",
+                },
+            },
+        ],
+    },
+    colors: ["#FFFFFF"],
+    chart: {
+        type: "bar",
+        stacked: true,
+        toolbar: {
+            show: true,
+        },
+        zoom: {
+            enabled: false,
+        },
+    },
+    dataLabels: {
+        enabled: false,
+    },
+    plotOptions: {
+        bar: {
+            horizontal: false,
+            borderRadius: 4,
+            dataLabels: {},
+        },
+    },
+    xaxis: {
+        type: "string",
+        categories: [],
+    },
+    yaxis: {
+        show: false,
+    },
+    legend: {
+        position: "bottom",
+    },
+    fill: {
+        opacity: 1,
+    },
+    tooltip: {
+        enabled: true,
+    },
+}
+
+onMounted(() => {
+    loadChartData()
+})
+watchEffect(() => loadChartData())
+
+function loadChartData(): void {
+    series = []
+    chartOptions.xaxis.categories = []
+    if (props.domain.professionalSkills != null) {
+        props.domain.professionalSkills.forEach((s) => {
+            chartOptions.xaxis.categories.push(s.shortName as never)
+        })
+    }
+
+    // Add data
+    series.push({
+        name: "Score",
+        data: DecayingAverageLogic.getAverageSkillOutcomeScores(
+            props.data,
+            props.domain
+        )?.map((d) => {
+            return {
+                y: d.decayingAverage.toFixed(3),
+                x: props.domain.professionalSkills?.at(d.skill)?.name,
+                fillColor: getMastery(d.masteryLevel)?.color,
+            }
+        }),
+    })
+}
+
+function getMastery(masteryId: number): MasteryLevel | undefined {
+    if (props.domain.masteryLevels == null) {
+        return undefined
+    }
+
+    return props.domain.masteryLevels.find(
+        (masteryLevel) => masteryLevel.id == masteryId
+    )
+}
+</script>
