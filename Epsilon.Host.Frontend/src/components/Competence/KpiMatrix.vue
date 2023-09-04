21b959c4691dd6c5fc0c2ecca79cca305af979c5
Neal Geilen <info@nealgeilen.nl>
2023-09-04 15:42:40 +0200

    Fix lint issue

--- Epsilon.Host.Frontend/src/components/Competence/KpiMatrix.vue
+++ Epsilon.Host.Frontend/src/components/Competence/KpiMatrix.vue
@@ -12,17 +12,22 @@
             </tr>
         </thead>
         <tbody>
-            <tr v-for="outcome of allOutcomes.sort()" :key="outcome">
+            <tr
+                v-for="outcome of allOutcomes.sort() as string[]"
+                :key="outcome">
                 <th>
                     {{ store.state.outcomes.find((o) => o.id == outcome).name }}
                 </th>
                 <KpiMatrixCell
                     v-for="submission of store.state.filterdSubmissions"
+                    :key="submission.assignmentUrl"
                     :result="
-                        submission.results.find((r) => r.outcome.id == outcome)
+                        submission.results?.find(
+                            (r) => r?.outcome?.id == outcome
+                        )
                     "
                     :criteria="
-                        submission.criteria.find((c) => c.id === outcome)
+                        submission.criteria?.find((c) => c?.id == outcome)
                     "></KpiMatrixCell>
             </tr>
         </tbody>
@@ -32,12 +37,13 @@
 import { computed } from "vue"
 import { useStore } from "vuex"
 import KpiMatrixCell from "@/components/Competence/KpiMatrixCell.vue"
+
 const store = useStore()
 
 const allOutcomes = computed(
     () =>
         store.state.filterdSubmissions
-            .flatMap((sub) => sub.criteria?.map((cr) => cr.id))
+            .flatMap((sub) => sub.criteria?.map((cr) => cr.id as string))
             .filter((value, index, self) => self.indexOf(value) === index)
     // .map((uniqueOutcome) => props.domain.)
 )
@@ -56,6 +62,7 @@ td div {
     border-bottom: 1px lightgray solid;
     width: 100%;
 }
+
 thead th {
     writing-mode: vertical-lr;
 }
