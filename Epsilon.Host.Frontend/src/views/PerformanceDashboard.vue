d1f0e0dfd0846fdd990a4875d84e8046d93a3a80
Neal Geilen <info@nealgeilen.nl>
2023-09-13 14:07:01 +0200

    Added styling of older version

--- Epsilon.Host.Frontend/src/views/PerformanceDashboard.vue
+++ Epsilon.Host.Frontend/src/views/PerformanceDashboard.vue
@@ -1,12 +1,39 @@
 <template>
-    <div>Performance dashboard</div>
-    <CompetenceGraph v-if="store.state.submissions"></CompetenceGraph>
-    <PersonalDevelopmentGraph
-        v-if="store.state.submissions"></PersonalDevelopmentGraph>
+    <div v-if="store.state.filterdSubmissions" class="performance-dashboard">
+        <CompetenceProfile
+            :submissions="store.state.filterdSubmissions"></CompetenceProfile>
+        <CompetenceProfileLegend></CompetenceProfileLegend>
+        <div></div>
+        <CompetenceGraph></CompetenceGraph>
+        <PersonalDevelopmentGraph></PersonalDevelopmentGraph>
+    </div>
 </template>
 
 <script lang="ts" setup>
 import CompetenceGraph from "@/components/Performance/CompetenceGraph.vue"
 import store from "@/store"
 import PersonalDevelopmentGraph from "@/components/Performance/PersonalDevelopmentGraph.vue"
+import CompetenceProfileLegend from "@/components/CompetenceProfileLegend.vue"
+import CompetenceProfile from "@/components/Competence/CompetenceProfile.vue"
 </script>
+
+<style scoped>
+.performance-dashboard {
+    grid-template-columns: 1fr;
+}
+
+.loading-icon {
+    width: 64px;
+    height: 64px;
+}
+
+@media screen and (min-width: 580px) {
+    .performance-dashboard {
+        display: grid;
+        grid-template-columns: 1fr 5fr 1fr;
+        gap: 2rem 0;
+        align-items: center;
+        justify-items: center;
+    }
+}
+</style>
