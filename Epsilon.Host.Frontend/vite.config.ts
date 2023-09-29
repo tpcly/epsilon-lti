460b4dbade8350b068c0d755637aec189d125364
Koen Janssen <mail@koenjanssen.dev>
2023-09-29 14:44:33 +0200

    Update docker-compose Co-authored-by: Ward Latten wardlatten@hotmail.nl

--- Epsilon.Host.Frontend/vite.config.ts
+++ Epsilon.Host.Frontend/vite.config.ts
@@ -15,11 +15,12 @@ export default ({ mode }: { mode: string }): UserConfigExport => {
             },
         },
         server: {
-            https: {
-                cert: certificate ? readFileSync(certificate) : undefined,
-                key: key ? readFileSync(key) : undefined,
-            },
-            port: 8000,
+            // https: {
+            //     cert: certificate ? readFileSync(certificate) : undefined,
+            //     key: key ? readFileSync(key) : undefined,
+            // },
+            http: {},
+            port: 80,
         },
         plugins: [vue()],
     })
