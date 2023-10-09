1afa708ecf830800399a0d96f2fb5cd2ccbae21d
Neal Geilen <info@nealgeilen.nl>
2023-10-09 13:57:14 +0200

    Added style definition for indentation and API explicit generated note (#50)
    * Set default spaces instead of tabs
    
    * changed name of api class
    
    * Linting fixes

--- Epsilon.Host.Frontend/src/components/SearchBox.vue
+++ Epsilon.Host.Frontend/src/components/SearchBox.vue
@@ -1,60 +1,60 @@
 <template>
-    <div class="searchbox">
-        <Combobox
-            :model-value="modelValue"
-            @update:model-value="$emit('update:modelValue', $event)">
-            <div class="searchbox-input">
-                <ComboboxInput
-                    :display-value="displayValue"
-                    :placeholder="placeholder"
-                    @change="query = $event.target.value" />
-                <ComboboxButton class="searchbox-list-arrow">
-                    <ChevronUpDownIcon aria-hidden="true" />
-                </ComboboxButton>
-            </div>
-            <ComboboxOptions v-if="items?.length > 0" class="searchbox-options">
-                <div
-                    v-if="filteredItems?.length === 0 && query !== ''"
-                    class="searchbox-options-item">
-                    No results found
-                </div>
+	<div class="searchbox">
+		<Combobox
+			:model-value="modelValue"
+			@update:model-value="$emit('update:modelValue', $event)">
+			<div class="searchbox-input">
+				<ComboboxInput
+					:display-value="displayValue"
+					:placeholder="placeholder"
+					@change="query = $event.target.value" />
+				<ComboboxButton class="searchbox-list-arrow">
+					<ChevronUpDownIcon aria-hidden="true" />
+				</ComboboxButton>
+			</div>
+			<ComboboxOptions v-if="items?.length > 0" class="searchbox-options">
+				<div
+					v-if="filteredItems?.length === 0 && query !== ''"
+					class="searchbox-options-item">
+					No results found
+				</div>
 
-                <ComboboxOption
-                    v-for="(item, id) in filteredItems"
-                    :key="id"
-                    v-slot="{ selected, active }"
-                    as="template"
-                    :value="item"
-                    class="searchbox-options-item">
-                    <li :class="{ 'searchbox-options-item-active': active }">
-                        {{ item.name }}
-                        <CheckIcon
-                            v-if="selected"
-                            class="searchbox-options-item-select-icon"
-                            aria-hidden="true" />
-                    </li>
-                </ComboboxOption>
-            </ComboboxOptions>
-        </Combobox>
-    </div>
+				<ComboboxOption
+					v-for="(item, id) in filteredItems"
+					:key="id"
+					v-slot="{ selected, active }"
+					as="template"
+					:value="item"
+					class="searchbox-options-item">
+					<li :class="{ 'searchbox-options-item-active': active }">
+						{{ item.name }}
+						<CheckIcon
+							v-if="selected"
+							class="searchbox-options-item-select-icon"
+							aria-hidden="true" />
+					</li>
+				</ComboboxOption>
+			</ComboboxOptions>
+		</Combobox>
+	</div>
 </template>
 
 <script lang="ts" setup>
 import { computed, defineProps, ref } from "vue"
 import {
-    Combobox,
-    ComboboxButton,
-    ComboboxInput,
-    ComboboxOptions,
-    ComboboxOption,
+	Combobox,
+	ComboboxButton,
+	ComboboxInput,
+	ComboboxOptions,
+	ComboboxOption,
 } from "@headlessui/vue"
 import { CheckIcon, ChevronUpDownIcon } from "@heroicons/vue/20/solid"
 
 const props = defineProps<{
-    items: Array<{ name?: string | null }> | null
-    modelValue: { name: string }
-    placeholder?: string
-    limit: number
+	items: Array<{ name?: string | null }> | null
+	modelValue: { name: string }
+	placeholder?: string
+	limit: number
 }>()
 
 const query = ref("")
@@ -62,108 +62,108 @@ const query = ref("")
 defineEmits(["update:modelValue"])
 
 const filteredItems = computed(() => {
-    if (props.items === null) {
-        return null
-    }
-    if (query.value === "") {
-        return props.items.slice(0, props.limit)
-    }
+	if (props.items === null) {
+		return null
+	}
+	if (query.value === "") {
+		return props.items.slice(0, props.limit)
+	}
 
-    return props.items
-        .filter((item) => {
-            if (!item.name) {
-                return false
-            }
+	return props.items
+		.filter((item) => {
+			if (!item.name) {
+				return false
+			}
 
-            return item.name
-                .toLowerCase()
-                .replace(/\s+/g, "")
-                .includes(query.value.toLowerCase().replace(/\s+/g, ""))
-        })
-        .slice(0, props.limit)
+			return item.name
+				.toLowerCase()
+				.replace(/\s+/g, "")
+				.includes(query.value.toLowerCase().replace(/\s+/g, ""))
+		})
+		.slice(0, props.limit)
 })
 
 function displayValue(item: { name: string }): string {
-    if (item) {
-        return item.name
-    }
+	if (item) {
+		return item.name
+	}
 
-    return ""
+	return ""
 }
 </script>
 
 <style scoped lang="scss">
 .searchbox {
-    position: relative;
-    width: 100%;
-    background-color: #fff;
-    border-radius: 7px;
+	position: relative;
+	width: 100%;
+	background-color: #fff;
+	border-radius: 7px;
 
-    &-input {
-        position: relative;
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        border: none;
-        font-weight: 400;
-        text-align: left;
-        width: 100%;
-        padding: 0.75rem;
+	&-input {
+		position: relative;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		border: none;
+		font-weight: 400;
+		text-align: left;
+		width: 100%;
+		padding: 0.75rem;
 
-        input {
-            width: 100%;
-            border: none;
-            outline: none;
-            font-size: 1rem;
-            border-radius: 6px;
-        }
-    }
+		input {
+			width: 100%;
+			border: none;
+			outline: none;
+			font-size: 1rem;
+			border-radius: 6px;
+		}
+	}
 
-    &-options {
-        position: absolute;
-        background-color: #fff;
-        width: 100%;
-        list-style-type: none;
-        border-radius: 6px;
-        border: 1px solid #d8d8d8;
-        text-align: left;
-        z-index: 40;
+	&-options {
+		position: absolute;
+		background-color: #fff;
+		width: 100%;
+		list-style-type: none;
+		border-radius: 6px;
+		border: 1px solid #d8d8d8;
+		text-align: left;
+		z-index: 40;
 
-        &-item {
-            padding: 1rem 1.5rem;
-            cursor: pointer;
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
+		&-item {
+			padding: 1rem 1.5rem;
+			cursor: pointer;
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
 
-            &-active {
-                background-color: #f2f3f8;
-            }
+			&-active {
+				background-color: #f2f3f8;
+			}
 
-            &-select-icon {
-                padding-left: 4px;
-                width: 20px;
-                height: 20px;
-            }
-        }
-    }
+			&-select-icon {
+				padding-left: 4px;
+				width: 20px;
+				height: 20px;
+			}
+		}
+	}
 
-    &-list-arrow {
-        background-color: transparent;
-        border: none;
-        border-radius: 0;
+	&-list-arrow {
+		background-color: transparent;
+		border: none;
+		border-radius: 0;
 
-        &:hover,
-        &:focus,
-        &:active {
-            border: none;
-            outline: none;
-        }
+		&:hover,
+		&:focus,
+		&:active {
+			border: none;
+			outline: none;
+		}
 
-        svg {
-            height: 20px;
-            max-height: 30px;
-        }
-    }
+		svg {
+			height: 20px;
+			max-height: 30px;
+		}
+	}
 }
 </style>
--- Epsilon.Host.Frontend/src/components/TopNavigation.vue
+++ Epsilon.Host.Frontend/src/components/TopNavigation.vue
@@ -1,26 +1,26 @@
 <template>
-    <div class="header">
-        <img alt="logo" class="header-logo" src="../assets/logo.png" />
-        <Row>
-            <Col :cols="9">
-                <!--TODO Component is used to selected users for upcoming feature-->
-                <!--                <div class="d-flex">-->
-                <!--                    <SearchBox-->
-                <!--                        v-model="store.state.currentUser"-->
-                <!--                        :items="store.state.users"-->
-                <!--                        :limit="5"-->
-                <!--                        placeholder="Student" />-->
-                <!--                </div>-->
-            </Col>
-            <Col :cols="3">
-                <SearchBox
-                    v-model="selectedTerm"
-                    :items="store.state.userTerms"
-                    :limit="10"
-                    placeholder="Term" />
-            </Col>
-        </Row>
-    </div>
+	<div class="header">
+		<img alt="logo" class="header-logo" src="../assets/logo.png" />
+		<Row>
+			<Col :cols="9">
+				<!--TODO Component is used to selected users for upcoming feature-->
+				<!--                <div class="d-flex">-->
+				<!--                    <SearchBox-->
+				<!--                        v-model="store.state.currentUser"-->
+				<!--                        :items="store.state.users"-->
+				<!--                        :limit="5"-->
+				<!--                        placeholder="Student" />-->
+				<!--                </div>-->
+			</Col>
+			<Col :cols="3">
+				<SearchBox
+					v-model="selectedTerm"
+					:items="store.state.userTerms"
+					:limit="10"
+					placeholder="Term" />
+			</Col>
+		</Row>
+	</div>
 </template>
 
 <script lang="ts" setup>
@@ -29,61 +29,61 @@ import Row from "@/components/LayoutRow.vue"
 import Col from "@/components/LayoutCol.vue"
 
 import { inject, ref, Ref, watch } from "vue"
-import { Api, EnrollmentTerm, HttpResponse } from "@/api"
+import { ApiGenerated, EnrollmentTerm, HttpResponse } from "@/api.generated"
 import { useStore } from "vuex"
 
-const api = inject<Api<unknown>>("api")
+const api = inject<ApiGenerated<unknown>>("api")
 const store = useStore()
 const selectedTerm: Ref<EnrollmentTerm | undefined> = ref(undefined)
 
 watch(selectedTerm, () => {
-    store.commit("setCurrentTerm", selectedTerm.value)
-    store.commit("filterSubmissions")
+	store.commit("setCurrentTerm", selectedTerm.value)
+	store.commit("filterSubmissions")
 })
 
 watch(store.state.currentUser, () => {
-    if (!store.state.currentUser?._id) {
-        return
-    }
+	if (!store.state.currentUser?._id) {
+		return
+	}
 
-    // Reset current term list
-    store.commit("setUserTerms", [])
+	// Reset current term list
+	store.commit("setUserTerms", [])
 
-    api?.filter
-        .participatedTermsList({
-            studentId: store.state.currentUser?._id,
-        })
-        .then((r: HttpResponse<EnrollmentTerm[]>) => {
-            store.commit("setUserTerms", r.data)
-            selectedTerm.value = store.state.currentTerm
-            store.commit("setCurrentTerm", store.state.userTerms[0])
-        })
+	api?.filter
+		.participatedTermsList({
+			studentId: store.state.currentUser?._id,
+		})
+		.then((r: HttpResponse<EnrollmentTerm[]>) => {
+			store.commit("setUserTerms", r.data)
+			selectedTerm.value = store.state.currentTerm
+			store.commit("setCurrentTerm", store.state.userTerms[0])
+		})
 })
 
 api?.filter
-    .participatedTermsList({
-        studentId: import.meta.env.VITE_USER_ID,
-    })
-    .then((r: HttpResponse<EnrollmentTerm[]>) => {
-        store.commit("setUserTerms", r.data)
-        store.commit("setCurrentTerm", store.state.userTerms[0])
-        selectedTerm.value = store.state.currentTerm
-    })
+	.participatedTermsList({
+		studentId: import.meta.env.VITE_USER_ID,
+	})
+	.then((r: HttpResponse<EnrollmentTerm[]>) => {
+		store.commit("setUserTerms", r.data)
+		store.commit("setCurrentTerm", store.state.userTerms[0])
+		selectedTerm.value = store.state.currentTerm
+	})
 </script>
 
 <style lang="scss" scoped>
 .header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 2rem 3rem;
-    background-color: #f2f3f8;
-    width: 100%;
-    border-radius: 0.5rem;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 2rem 3rem;
+	background-color: #f2f3f8;
+	width: 100%;
+	border-radius: 0.5rem;
 
-    &-logo {
-        height: 5rem;
-        object-fit: contain;
-    }
+	&-logo {
+		height: 5rem;
+		object-fit: contain;
+	}
 }
 </style>
