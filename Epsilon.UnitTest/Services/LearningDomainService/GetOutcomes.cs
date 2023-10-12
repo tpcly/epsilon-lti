cd87d28ee31fbeffc923d42c838b1ff0762c0aa9
Neal Geilen <info@nealgeilen.nl>
2023-10-12 11:26:34 +0200

    Test

---
+++ Epsilon.UnitTest/Services/LearningDomainService/GetOutcomes.cs
@@ -0,0 +1,20 @@
+﻿using Epsilon.Abstractions.Services;
+using Moq;
+
+namespace Epsilon.UnitTest.Services.LearningDomainService;
+
+public class GetOutcomes
+{
+    [Fact]
+    public void GetOutcomes_givenValidDataStructure_thenResultIsAValidDomainModel()
+    {
+        //Arrange
+        var learningDomainServiceMock = new Mock<ILearningDomainService>();
+        learningDomainServiceMock.Setup(m => m.)
+        //Act
+        var result = learningDomainServiceMock.Object.GetOutcomes();
+
+        //Assert
+        Assert.T(result)
+    }
+}
---
+++ Epsilon.UnitTest/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace Epsilon.UnitTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
