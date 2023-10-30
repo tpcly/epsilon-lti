61bfcc8b8a326cdaba1c3a830ced7acedf564035
Neal Geilen <info@nealgeilen.nl>
2023-10-30 13:14:01 +0100

    Mask all possible texts and inputs

--- Epsilon.Host.Frontend/posthog.ts
+++ Epsilon.Host.Frontend/posthog.ts
@@ -5,7 +5,8 @@ export class Posthog {
 		posthog.init("phc_wdX1JI1sEoh51qmTetv3MIvy5Sa8QywWgCrxTKzcCDI", {
 			api_host: "https://eu.posthog.com",
 			session_recording: {
-				maskTextSelector: "*", // Masks all text elements (not including inputs)
+				maskAllInputs: true,
+				maskTextSelector: "*",
 			},
 		})
 	}
