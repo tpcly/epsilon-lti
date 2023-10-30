11ad306d7754210899386540dc937fd375029ea3
Neal Geilen <info@nealgeilen.nl>
2023-10-30 09:57:49 +0100

    Disable notices

--- Epsilon.Abstractions/Components/IWordCompetenceComponent.cs
+++ Epsilon.Abstractions/Components/IWordCompetenceComponent.cs
@@ -4,5 +4,6 @@ namespace Epsilon.Abstractions.Components;
 
 public interface IWordCompetenceComponent
 {
+    // ReSharper disable once UnusedMemberInSuper.Global
     public void AddToWordDocument(MainDocumentPart mainDocumentPart);
 }
