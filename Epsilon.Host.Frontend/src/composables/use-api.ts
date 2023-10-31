c86f92c71f289b0a02151db84ae14e627ca960fa
Jelle Maas <typiqally@gmail.com>
2023-10-31 12:35:29 +0100

    Use ref directly in header param

--- Epsilon.Host.Frontend/src/composables/use-api.ts
+++ Epsilon.Host.Frontend/src/composables/use-api.ts
@@ -2,13 +2,13 @@ import { Api } from "~/api.generated"
 
 export const useApi = () => {
     const runtimeConfig = useRuntimeConfig()
-    const token = useState<string>("id_token").value
+    const token = useState("id_token")
 
     return new Api({
         baseUrl: runtimeConfig.public.apiEndpoint,
         baseApiParams: {
             headers: {
-                "Authorization": `Bearer ${token}`,
+                "Authorization": `Bearer ${token.value}`,
             },
         },
     })
