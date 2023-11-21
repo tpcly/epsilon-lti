897426de43aadc50f62c22079450879143c7adf9
Neal Geilen <info@nealgeilen.nl>
2023-11-21 15:31:40 +0100

    Added LearningDomainServiceTests.cs

---
+++ Epsilon.UnitTest/Services/LearningDomainServiceTests.cs
@@ -0,0 +1,87 @@
+﻿using Epsilon.Abstractions;
+using Epsilon.Services;
+using Moq;
+using Tpcly.Persistence.Abstractions;
+
+namespace Epsilon.UnitTest.Services;
+
+public class LearningDomainServiceTests
+{
+    private readonly Mock<IReadOnlyRepository<LearningDomain>> _learningDomainRepositoryMock = new Mock<IReadOnlyRepository<LearningDomain>>();
+    private readonly Mock<IReadOnlyRepository<LearningDomainOutcome>> _learningDomainOutcomeRepositoryMock = new Mock<IReadOnlyRepository<LearningDomainOutcome>>();
+    private readonly IEnumerable<LearningDomainOutcome> _outcomes = TestDataGenerator.GenerateRandomLearningDomainOutcomes(10);
+    private LearningDomain _learningDomain = TestDataGenerator.GenerateRandomLearningDomain();
+    private readonly LearningDomainService _learningDomainService;
+
+    public LearningDomainServiceTests()
+    {
+        _learningDomainService = new LearningDomainService(_learningDomainRepositoryMock.Object, _learningDomainOutcomeRepositoryMock.Object);
+    }
+
+    [Fact]
+    public async Task GetOutcomes_ReturnsExpectedOutcomes()
+    {
+        // Arrange
+        _learningDomainOutcomeRepositoryMock.Setup(static repo => repo.AllToListAsync(null, It.IsAny<string[]?>(), It.IsAny<int?>(), It.IsAny<int?>())).ReturnsAsync(_outcomes);
+
+        // Act
+        var result = await _learningDomainService.GetOutcomes();
+
+        // Assert
+        Assert.Equal(_outcomes, result);
+    }
+
+    [Fact]
+    public async Task GetDomain_ReturnsExpectedDomain()
+    {
+        _learningDomain = TestDataGenerator.GenerateRandomLearningDomain();
+        // Arrange
+        var domainName = _learningDomain.Id;
+        _learningDomainRepositoryMock.Setup(repo => repo.SingleOrDefaultAsync(d => d.Id == domainName, It.IsAny<string[]>())).ReturnsAsync(_learningDomain);
+
+        // Act
+        var result = await _learningDomainService.GetDomain(domainName);
+
+        // Assert
+        Assert.Equal(_learningDomain, result);
+        Assert.NotNull(_learningDomain.ValuesSet);
+        Assert.NotNull(_learningDomain.RowsSet);
+        Assert.Null(_learningDomain.ColumnsSet);
+    }
+    
+    
+    [Fact]
+    public async Task GetDomain_ReturnsExpectedDomainWithColumnTypes()
+    {
+        _learningDomain = TestDataGenerator.GenerateRandomLearningDomain();
+        _learningDomain.ColumnsSet = TestDataGenerator.GenerateRandomLearningDomainTypeSet();
+        // Arrange
+        var domainName = _learningDomain.Id;
+        _learningDomainRepositoryMock.Setup(repo => repo.SingleOrDefaultAsync(d => d.Id == domainName, It.IsAny<string[]>())).ReturnsAsync(_learningDomain);
+
+        // Act
+        var result = await _learningDomainService.GetDomain(domainName);
+
+        // Assert
+        Assert.Equal(_learningDomain, result);
+        Assert.NotNull(_learningDomain.ValuesSet);
+        Assert.NotNull(_learningDomain.RowsSet);
+        Assert.NotNull(_learningDomain.ColumnsSet);
+    }
+
+
+    [Fact]
+    public async Task GetDomain_ReturnsColumnNull()
+    {
+        // Arrange
+        var domainName = _learningDomain.Id;
+        _learningDomainRepositoryMock.Setup(repo => repo.SingleOrDefaultAsync(d => d.Id == domainName, It.IsAny<string[]>())).ReturnsAsync(_learningDomain);
+
+        // Act
+        var result = await _learningDomainService.GetDomain(domainName);
+
+        // Assert
+        Assert.Equal(_learningDomain, result);
+        Assert.Null(_learningDomain.ColumnsSet);
+    }
+}
---
+++ Epsilon.UnitTest/TestDataGenerator.cs
@@ -0,0 +1,57 @@
+﻿using Epsilon.Abstractions;
+
+namespace Epsilon.UnitTest;
+
+public static class TestDataGenerator
+{
+    private static readonly Random s_random = new Random();
+
+    public static LearningDomainType GenerateRandomLearningDomainType()
+    {
+        return new LearningDomainType
+        {
+            Name = $"Type {s_random.Next(1, 100)}", ShortName = $"T{s_random.Next(1, 100)}", HexColor = $"#{s_random.Next(0x1000000):X6}", Order = s_random.Next(1, 100),
+        };
+    }
+
+    public static LearningDomainTypeSet GenerateRandomLearningDomainTypeSet()
+    {
+        return new LearningDomainTypeSet
+        {
+            Types = new List<LearningDomainType> { GenerateRandomLearningDomainType(), GenerateRandomLearningDomainType(), GenerateRandomLearningDomainType(), },
+        };
+    }
+
+    public static LearningDomain GenerateRandomLearningDomain()
+    {
+        return new LearningDomain
+        {
+            Id = $"Domain {s_random.Next(1, 100)}",
+            RowsSet = GenerateRandomLearningDomainTypeSet(),
+            ValuesSet = GenerateRandomLearningDomainTypeSet(),
+        };
+    }
+
+    public static LearningDomainOutcome GenerateRandomLearningDomainOutcome()
+    {
+        return new LearningDomainOutcome
+        {
+            TenantId = Guid.NewGuid(),
+            Row = GenerateRandomLearningDomainType(),
+            Column = GenerateRandomLearningDomainType(),
+            Value = GenerateRandomLearningDomainType(),
+            Name = $"Outcome {s_random.Next(1, 100)}",
+        };
+    }
+
+    public static List<LearningDomainOutcome> GenerateRandomLearningDomainOutcomes(int count)
+    {
+        var outcomes = new List<LearningDomainOutcome>();
+        for (var i = 0; i < count; i++)
+        {
+            outcomes.Add(GenerateRandomLearningDomainOutcome());
+        }
+
+        return outcomes;
+    }
+}
