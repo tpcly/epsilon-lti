0d8cea311dbe742fa6268a434587deca799bf2e7
Neal Geilen <info@nealgeilen.nl>
2023-12-11 12:13:55 +0100

    Add a Unit Testing framework (#121)
    * Test
    
    * start
    
    * Test
    
    * Added CI step for unit testing
    
    * CI update
    
    * Added LearningDomainServiceTests.cs
    
    * CI update
    
    * CI update
    
    * Added tests for user access
    
    * Bug fix
    
    * Added test for ReturnsCorrectDomains()
    
    * Added a test that will validate the format of the generated word file
    
    * Update
    
    * Merge
    
    * Update yaml format

---
+++ Epsilon.UnitTest/Services/AuthorizationUserTests.cs
@@ -0,0 +1,59 @@
+﻿using System.Globalization;
+using Epsilon.Abstractions;
+using Epsilon.Abstractions.Services;
+using Epsilon.Services;
+using Moq;
+
+namespace Epsilon.UnitTest.Services;
+
+public class AuthorizationUserTests
+{
+    private readonly Mock<IFilterService> _mockFilterService;
+    private readonly Mock<ICanvasUserSessionAccessor> _mockSessionAccessor;
+    private readonly AuthorizationUser _authorizationUser;
+
+    public AuthorizationUserTests()
+    {
+        _mockFilterService = new Mock<IFilterService>();
+        _mockSessionAccessor = new Mock<ICanvasUserSessionAccessor>();
+        _authorizationUser = new AuthorizationUser(_mockFilterService.Object, _mockSessionAccessor.Object);
+    }
+
+    [Fact]
+    public async Task HasCurrentUserAccessToUser_ReturnsTrue_WhenUserHasAccess()
+    {
+        // Arrange
+        var users = TestDataGenerator.GenerateUsers(10);
+        var user = users.First();
+
+        _mockFilterService.Setup(static fs => fs.GetAccessibleStudents()).ReturnsAsync(users);
+        _mockSessionAccessor.Setup(static sa => sa.GetSessionAsync()).ReturnsAsync(new CanvasUserSession(0,int.Parse(user.LegacyId!, CultureInfo.InvariantCulture), false));
+
+        // Act
+        var result = await _authorizationUser.HasCurrentUserAccessToUser(user.LegacyId ?? "");
+
+        // Assert
+        Assert.NotNull(user.LegacyId);
+        Assert.True(result);
+    }
+    
+    
+    [Fact]
+    public async Task HasCurrentUserAccessToUser_ReturnsTrue_TeacherHasAccessToStudent()
+    {
+        // Arrange
+        var users = TestDataGenerator.GenerateUsers(10);
+        var user = users.First();
+        var currentUser = users.Last();
+
+        _mockFilterService.Setup(static fs => fs.GetAccessibleStudents()).ReturnsAsync(users);
+        _mockSessionAccessor.Setup(static sa => sa.GetSessionAsync()).ReturnsAsync(new CanvasUserSession(0,int.Parse(user.LegacyId!, CultureInfo.InvariantCulture), true));
+
+        // Act
+        var result = await _authorizationUser.HasCurrentUserAccessToUser(currentUser.LegacyId ?? "");
+
+        // Assert
+        Assert.NotNull(user.LegacyId);
+        Assert.True(result);
+    }
+}
