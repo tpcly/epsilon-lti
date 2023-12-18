1e5c21429a51412b1f7b0f6df75e775acafebf4f
SaamieVincken <saamievincken@protonmail.com>
2023-12-18 21:01:21 +0100

    Fix naming issues in kpi matrix component

--- Epsilon/Components/KpiMatrixComponent.cs
+++ Epsilon/Components/KpiMatrixComponent.cs
@@ -12,7 +12,7 @@ public class KpiMatrixComponent : AbstractCompetenceComponent
     public KpiMatrixComponent(IAsyncEnumerable<LearningDomainSubmission> submissions, IEnumerable<LearningDomain?> domains)
         : base(submissions, domains)
     {
-        
+
     }
 
     public override async Task<Body> AddToWordDocument(MainDocumentPart mainDocumentPart)
@@ -20,15 +20,12 @@ public class KpiMatrixComponent : AbstractCompetenceComponent
         var body = new Body();
         // Create a table, with rows for the outcomes and columns for the assignments.
         var table = new Table();
-        
+
         var assignments = await Submissions.Select(static e => e.Assignment).ToListAsync();
-        
+
         // Set table properties for formatting.
         table.AppendChild(new TableProperties(
-            new TableWidth
-            {
-                Width = "0", Type = TableWidthUnitValues.Auto,
-            }));
+            new TableWidth { Width = "0", Type = TableWidthUnitValues.Auto, }));
 
         // Calculate the header row height based on the longest assignment name.
         var headerRowHeight = 0;
@@ -47,13 +44,10 @@ public class KpiMatrixComponent : AbstractCompetenceComponent
         {
             headerRowHeight *= 111;
         }
-        
+
         // Create the table header row.
         var headerRow = new TableRow();
-        headerRow.AppendChild(new TableRowProperties(new TableRowHeight
-        {
-            Val = (UInt32Value)(uint)headerRowHeight,
-        }));
+        headerRow.AppendChild(new TableRowProperties(new TableRowHeight { Val = (UInt32Value)(uint)headerRowHeight, }));
 
         // Empty top-left cell.
         headerRow.AppendChild(CreateTableCellWithBorders("2500", new Paragraph(new Run(new Text("")))));
@@ -61,7 +55,7 @@ public class KpiMatrixComponent : AbstractCompetenceComponent
         foreach (var assignment in assignments)
         {
             var cell = CreateTableCellWithBorders("100");
-            
+
             if (assignment != null)
             {
                 if (cell.FirstChild != null)
@@ -85,58 +79,49 @@ public class KpiMatrixComponent : AbstractCompetenceComponent
 
         var listOfOutcomes = Submissions
             .SelectMany(static e => e.Results
-                                     .Select(static result => new { result.Outcome.Id, result.Outcome,  })
+                                     .Select(static result => new { result.Outcome.Id, result.Outcome, })
                                      .ToAsyncEnumerable());
 
         // Add the outcome rows.
-        await foreach (var outcome in listOfOutcomes.OrderByDescending(static o => o.Outcome.Name))
+        await foreach (var outcome in listOfOutcomes.OrderByDescending(static o => o.Outcome.Value))
         {
             var row = new TableRow();
 
             // Add the outcome title cell.
-            row.AppendChild(CreateTableCellWithBorders("2500", new Paragraph(new Run(new Text(outcome.Outcome.Name)))));
+            row.AppendChild(CreateTableCellWithBorders("2500", new Paragraph(new Run(new Text(outcome.Outcome.Value.Name)))));
 
             // Add the assignment cells.
-             foreach (var assignment in assignments)
+            foreach (var assignment in assignments)
             {
-                var outcomeAssignment = assignment.Outcomes.FirstOrDefault(o => o.Id == outcome.Key);
-                var cell = CreateTableCellWithBorders("100");
-
-                // Set cell color based on GradeStatus.
-                var fillColor = outcomeAssignment != null
-                    ? outcomeAssignment.GradeStatus.Color
-                    : assignments.IndexOf(assignment) % 2 == 0
-                        ? "ffffff"
-                        : "d3d3d3";
-
-                cell.FirstChild?.Append(new Shading
+                if (assignment == outcome.Outcome.Name)
                 {
-                    Fill = fillColor,
-                });
+                    var outcomeAssignment = outcome.Outcome;
+                    
+                    var cell = CreateTableCellWithBorders("100");
 
+                    // Set cell color based on GradeStatus.
+                    var fillColor = outcomeAssignment.Value.HexColor;
 
-                cell.FirstChild?.Append(new Shading
-                {
-                    Fill = fillColor,
-                });
+                    cell.FirstChild?.Append(new Shading { Fill = fillColor, });
 
-                // Add an empty text element since we're using color instead of text.
-                cell.Append(new Paragraph(new Run(new Text(""))));
-                row.AppendChild(cell);
+                    // Add an empty text element since we're using color instead of text.
+                    cell.Append(new Paragraph(new Run(new Text(""))));
+                    row.AppendChild(cell);
+                }
             }
 
             table.AppendChild(row);
         }
 
-
-        body.AppendChild(GetLegend());
+        body.AppendChild(await GetLegend());
         body.Append(new Paragraph(new Run(new Text(""))));
         body.AppendChild(table);
 
-        mainDocumentPart.Document.AppendChild(body);
+        return mainDocumentPart.Document.AppendChild(body);
     }
     
-    private OpenXmlElement GetLegend()
+
+    private Task<OpenXmlElement> GetLegend()
     {
         var table = new Table();
         foreach (var status in GradeStatus)
@@ -158,8 +143,7 @@ public class KpiMatrixComponent : AbstractCompetenceComponent
 
         return table;
     }
-
-
+    
     private static TableCell CreateTableCellWithBorders(string? width, params OpenXmlElement[] elements)
     {
         var cell = new TableCell();
