b0757173194069de44e58eba594ad437c99950e9
Neal Geilen <info@nealgeilen.nl>
2024-02-13 16:06:47 +0100

    General small optimizations (#332)
    * Added CODEOWNERS
    
    * Improved auto release notes
    
    * Order students by user name
    
    * Show clear icon on student selection
    
    * Added auto tag updater for package.json file
    
    * Change

--- Epsilon/Services/FilterService.cs
+++ Epsilon/Services/FilterService.cs
@@ -106,6 +106,6 @@ public class FilterService : IFilterService
 
                 return er.User?.LegacyId == canvasUser?.UserId.ToString(CultureInfo.InvariantCulture) && er.Type == "StudentEnrollment";
             }
-        ).Select(static er => er.User!).DistinctBy(static u => u.LegacyId) ?? Array.Empty<User>();
+        ).Select(static er => er.User!).DistinctBy(static u => u.LegacyId).OrderBy(static u => u.Name);
     }
 }
