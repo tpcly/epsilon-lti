ee57f79ac60728e650a1b8ec139712083904e803
Neal Geilen <info@nealgeilen.nl>
2025-01-31 15:38:18 +0100

    Add description of outcomes to db table.

--- Epsilon.Host.WebApi/Data/Migrations/20250131143707_AddDescriptionToDomainOutcomes.Designer.cs
+++ Epsilon.Host.WebApi/Data/Migrations/20250131143707_AddDescriptionToDomainOutcomes.Designer.cs
@@ -4,6 +4,7 @@ using Epsilon.Host.WebApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Epsilon.Host.WebApi.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250131143707_AddDescriptionToDomainOutcomes")]
+    partial class AddDescriptionToDomainOutcomes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
